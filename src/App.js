4b6e9fc8f6e726a9b6faf377d29e1720035c49cc
Pranav Vishnu <pranavvishnur@gmail.com>
2017-03-23 14:37:12 -0400

    fix merge error

--- src/App.js
+++ src/App.js
@@ -8,17 +8,10 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-<<<<<<< HEAD
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-=======
           <h2>PennApps Site</h2>
         </div>
         <p className="App-intro">
           Run <code>npm start</code>, edit <code>src/App.js</code> and save to reload.
->>>>>>> c5681637dbcfc190e44d8cb235ce8d2a527fa289
         </p>
       </div>
     );
