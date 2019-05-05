99f37b90e5393a77791565632393f08575958682
Yash S <ys.thedood@gmail.com>
2019-05-05 11:13:49 -0400

    Building site for gh-pages

---
+++ build/precache-manifest.40a99cad211c04d76d12bc0df49fbde9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0def81be9bae91ebd2fbf37149f17256",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05c30cd837e6b8f455ae",
+    "url": "/static/css/main.c3495d57.chunk.css"
+  },
+  {
+    "revision": "ee1f24754424bf98056e",
+    "url": "/static/js/2.5851de72.chunk.js"
+  },
+  {
+    "revision": "05c30cd837e6b8f455ae",
+    "url": "/static/js/main.b68884c4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "081ac82f1f89beec58faf0a6ee5ba563",
+    "url": "/static/media/logo.081ac82f.png"
+  }
+]);
